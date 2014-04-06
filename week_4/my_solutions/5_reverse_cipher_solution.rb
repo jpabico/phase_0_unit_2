9893ef883d881e7d9e143594d7c97d4957e0c54b
Jon Pabico <jbpabico@gmail.com>
2014-04-05 17:09:12 -0700

    explained each line with comment

--- week_4/my_solutions/5_reverse_cipher_solution.rb
+++ week_4/my_solutions/5_reverse_cipher_solution.rb
@@ -1,7 +1,7 @@
 # U2.W4: Refactor Cipher Solution
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 
 
 # 1. Solution
@@ -10,25 +10,28 @@
 # Also make sure each step is necessary. If you don't think it's necessary
 # Try implementing the code without it. 
 
-def translate_to_cipher(sentence)
-    alphabet = ('a'..'z').to_a
+def translate_to_cipher(sentence)   # define method with string input
+    alphabet = ('a'..'z').to_a   # an array consisting of the letters a through z as strings
+
+# alphabet.rotate(4) rotates the elements of alphabet by setting the element at index 4 as the first element of a new array
+#
     cipher = Hash[alphabet.zip(alphabet.rotate(4))]
-    spaces = ["@", "#", "$", "%", "^", "&", "*"]
+    spaces = ["@", "#", "$", "%", "^", "&", "*"]   # an array of characters that represent spaces
     
-    original_sentence = sentence.downcase
-    encoded_sentence = []
-    original_sentence.each_char do |element|
-      if cipher.include?(element)
-        encoded_sentence << cipher[element]
-      elsif element == ' '
-        encoded_sentence << spaces.sample
-      else 
-        encoded_sentence << element
-      end
-     end
+    original_sentence = sentence.downcase   # takes string input and makes sure everything is lower case
+    encoded_sentence = []   # define an empty array to hold encoded sentence
+    original_sentence.each_char do |element|   # for each character in the input string, do the following...
+      if cipher.include?(element)   # if the character is present as a key in the cipher hash...
+        encoded_sentence << cipher[element]   # push that key's value into the encoded_sentence array
+      elsif element == ' '   # if the character in the original sentence is a space...
+        encoded_sentence << spaces.sample   # pick a random element from the spaces array and push it into the encoded_sentence array
+      else    # if none of the above are true, do the following...
+        encoded_sentence << element   # push the character itself into the encoded_sentence array
+      end   # end of conditional statements
+     end   # end of do statement
     
-    return encoded_sentence.join
-end
+    return encoded_sentence.join   # return the elements of the encoded_sentence array as one string 
+end   # end of method
 
 
 # Questions:
