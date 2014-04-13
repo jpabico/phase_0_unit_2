ca8051c435d0e2ac9c74e9626d0cbc3cd29ae9d6
Jon Pabico <jbpabico@gmail.com>
2014-04-13 12:07:56 -0700

    finished refactoring

--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -1,21 +1,37 @@
 # U2.W5: Bakery Challenge GPS
 
-# I worked on this challenge with: 
-
-
-
-
-
+# I worked on this challenge with: Keenan Turner
 
 # Our Refactored Solution
 
+def bakery_num(num_of_people, fav_food)
+    my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
+    can_make = false
 
+    my_list.each_key {|food_item| can_make = true if food_item == fav_food }
+    raise ArgumentError.new("You can't make that food") if can_make == false
+        
+    fav_food_qty = my_list.values_at(fav_food)[0]
+    if num_of_people % fav_food_qty == 0
+        num_of_food = num_of_people / fav_food_qty
+        return "You need to make #{num_of_food} #{fav_food}(s)."
+    else
+        if fav_food == "cake"
+            cake_qty = num_of_people / my_list["cake"]   # first calculate cake required
+            num_of_people %= my_list["cake"]
+            pie_qty = num_of_people / my_list["pie"]   # second calculate pie required
+            num_of_people %= my_list["pie"]
+        else
+            pie_qty = num_of_people / my_list["pie"]   # first calculate pie required
+            num_of_people %= my_list["pie"]
+            cake_qty = num_of_people / my_list["cake"]   #second calculate cake required
+            num_of_people %= my_list["cake"]
+        end
 
-
-
-
-
-
+        cookie_qty = num_of_people   #either way, calculate cookies required last
+        return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+    end
+end
 
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
 # These are the tests to ensure it's working. 
@@ -28,12 +44,8 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 # p bakery_num(3, "apples") # this will raise an ArgumentError
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
-p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
-
-
-
+p bakery_num(41, "cake") == "You need to make 0 pie(s), 6 cake(s), and 5 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
 
 #  Reflection 
-
 
 
