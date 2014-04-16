94858f53b30a300d621bbbae63970887b4a22016
Jon Pabico <jbpabico@gmail.com>
2014-04-16 15:39:24 -0700

    found initial solution

--- week_5/10_BONUS_rpn/my_solution.rb
+++ week_5/10_BONUS_rpn/my_solution.rb
@@ -1,20 +1,51 @@
 # U2.W5: Implement a Reverse Polish Notation Calculator
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
-
+# Input: a string of numbers and math operators
+# Output: a correctly evaluated number
+# Steps: define class called PRNCalculator
+# initialize an instance variable called "str_to_evaluate"
+# convert str_to_evaluate into array
+# define instance method called "evaluate"
+# return value of the original string evaluated 
 
 # 3. Initial Solution
 
 class RPNCalculator
-  # Define your methods, here!
+	def initialize(str_to_evaluate)
+		if str_to_evaluate.class != String
+			raise ArgumentError("please enter a string")
+		else
+			@str_to_evaluate = str_to_evaluate
+			@array_to_eval = @str_to_evaluate.split(" ")
+		end
+	end
+
+	def evaluate
+		if @array_to_eval[2] == "+"
+			return @array_to_eval[0].to_i + @array_to_eval[1].to_i
+		elsif @array_to_eval[2] == "-"
+			return @array_to_eval[0].to_i - @array_to_eval[1].to_i
+		elsif @array_to_eval[2] == "*"
+			return @array_to_eval[0].to_i * @array_to_eval[1].to_i
+		else
+			return "Not a valid mathematical operator"
+		end
+	end
 end
+
+# test_1 = RPNCalculator.new("2 5 +")
+# a = test_1.evaluate
+# test_2 = RPNCalculator.new("2 5 -")
+# a = test_2.evaluate
+# test_3 = RPNCalculator.new("2 5 *")
+# a = test_3.evaluate
+# test_4 = RPNCalculator.new("2 5 ^")
+# a = test_4.evaluate
 
 
 
@@ -26,10 +57,46 @@ end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-
-
+# test_1 = RPNCalculator.new("-1")
+# p test_1.evaluate == -1
+# test_2 = RPNCalculator.new("3 5 +")
+# p test_2.evaluate == 8
+# test_3 = RPNCalculator.new("3 5 -")
+# p test_3.evaluate == -2
+# test_4 = RPNCalculator.new("3 5 *")
+# p test_4.evaluate == 15
 
 
 
 
 # 5. Reflection 
+
+# class RPNCalculator
+#   def evaluate (input)
+
+#     raise ArgumentError.new("input must be a string") unless input.class.to_s == "String"
+#     stack = []
+
+#     input.split(" ").each do |element|
+#       case element
+#       when /[0-9]/
+#         stack << element
+#       when "+"
+#         stack << (stack.pop.to_i + stack.pop.to_i)
+#       when "*"
+#         stack << (stack.pop.to_i * stack.pop.to_i)
+#       when "-"
+#         stack << (-1*(stack.pop.to_i - stack.pop.to_i))
+#       else
+#         raise ArgumentError.new("input contains invalid characters")
+#       end
+#     end
+
+#     stack[0].to_i
+
+#   end
+# end
+
+# test_1 = RPNCalculator.new
+# a = test_1.evaluate("2 5 +")
+# puts a
--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,102 +1,137 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
-
+# I worked on this challenge with Jon Pabico.
+## Directions
+=begin 
+1. Format the code to indent it properly
+2. Debug the program
+Run the code from the console. You should encounter 5 errors before it will run completely (excluding the bonus - uncomment the line to work on it). Read each error carefully and fix it. 
+3. Ensure each method is working as expected. You may need to create or modify methods to make the program run properly.
+4. Write tests using [Driver Test Code](../../references/driver_code.md) or `assert` statements (make sure you have your assert method defined!)
+5. Interact with the code by writing at least three more different method calls. Did they return what you expected?
+6. [Reflect](../../references/reflection_guidelines.md)
+7. Sync your changes (push your solution) to Github
+=end
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+    attr_reader :contents
 
 # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+    def initialize
+        @contents = []
+        @open = true
+    end
+
+    def open
+        @open = true
+    end
+
+    def close
+        @open = false
+    end 
+
+    def add_item(item)
+        if item.clean == false
+            puts "Clean item first!"
+        else
+            @contents << item
+        end
+
+    end
+
+    def remove_item(item = @contents.pop) #what is `#pop` doing?
+        @contents.delete(item)
+        item
+    end
+
+    def dump  # what should this method return?
+       if @contents.empty? == false
+           puts "Your drawer is now empty."
+           return @contents.clear
+       else
+           puts "Your drawer is already empty."
+           return @contents
+       end 
+    end
+
+    def view_contents
+        if @contents.empty? == true
+            puts "Your drawer contains nothing."
+        else
+            puts "The drawer contains:"
+            @contents.each {|silverware| puts "- " + silverware.type }
+        end
+    end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+    attr_reader :type, :clean
 
 # Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
+    def initialize(type)
+        @type = type
+        @clean = true 
+    end
+
+    def eat
+        puts "eating with the #{@type}"
+        @clean = false
+    end
+    
+    def clean_silverware
+        @clean = true
+    end
 end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
-end
 
 knife1 = Silverware.new("knife")
+spoon = Silverware.new("spoon")
+fork = Silverware.new("fork")
 
 silverware_drawer = Drawer.new
 silverware_drawer.add_item(knife1) 
-silverware_drawer.add_item(Silverware.new("spoon"))
-silverware_drawer.add_item(Silverware.new("fork")) 
+silverware_drawer.add_item(spoon)
+silverware_drawer.add_item(fork) 
 silverware_drawer.view_contents
 
-silverware_drawer.remove_item
+removed_fork = silverware_drawer.remove_item(fork)
+removed_fork.eat
+removed_fork.clean_silverware
+silverware_drawer.add_item(removed_fork)
 silverware_drawer.view_contents
+
 sharp_knife = Silverware.new("sharp_knife")
-
-
 silverware_drawer.add_item(sharp_knife)
-
-silverware_drawer.view_contents
-
 removed_knife = silverware_drawer.remove_item(sharp_knife)
 removed_knife.eat
-removed_knife.clean_silverware 
+silverware_drawer.view_contents
+
+removed_spoon = silverware_drawer.remove_item(spoon)
+removed_spoon.eat
+silverware_drawer.add_item(removed_spoon)
+
 
 silverware_drawer.view_contents
 silverware_drawer.dump
 silverware_drawer.view_contents #What should this return?
 
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-fork.eat
+
 
 #BONUS SECTION
-# puts fork.clean
+ puts fork.clean
 
 # DRIVER TESTS GO BELOW THIS LINE
+def assert 
+    raise "Fail" unless yield
+end
 
-
-
-
-
-
-# 5. Reflection 
+    assert { silverware_drawer.dump.empty?}
+assert { silverware_drawer.class != Silverware}
+assert { silverware_drawer.remove_item("fork") == "fork"}
